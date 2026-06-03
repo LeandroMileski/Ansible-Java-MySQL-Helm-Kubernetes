3f1978e7ab5c5f415d09f1afd04a8d6269eb8d91
root <root@DESKTOP-6MRB30L.localdomain>
2026-06-03 12:51:32 +0100

    feat: define your IP to allow SSH access.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -38,3 +38,9 @@ variable "image" {
   type        = string
   default     = "linode/ubuntu24.04"
 }
+
+variable "my_ip" {
+  description = "Define your IP to allow SSH access."
+  type        = string
+  default     = "0.0.0.0"
+}
